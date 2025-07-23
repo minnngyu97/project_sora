1478f2a2b331be82ac85c0dc19ed9e54df7e2215
hansusanna <undo@gmail.com>
2025-07-23 17:18:41 +0900

    스와이퍼 실행식 수정

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,15 +1,11 @@
-$(function(){
-
-    const slide1 = new Swiper('.slide1',{
-        pagination:{
-            el:".swiper-pagination",
-            clickable: true
-        },
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false
-        },
-        speed: 1000
-    })
-
-})
+const slide1 = new Swiper(".slide1", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  speed: 1000,
+});
