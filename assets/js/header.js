3f590d716eb64619c8c3045098551a7b45021769
minnngyu97 <mggkxmjw@gmail.com>
2025-07-24 00:05:07 +0900

    헤더 메뉴, 슬라이더, 태그 위치 등 수정

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,26 +1,26 @@
-$(function(){
+// $(function(){
 
-    let lastScroll = 0;
+//     let lastScroll = 0;
 
-    $(window).scroll(function(){
-        curr = $(this).scrollTop();
+//     $(window).scroll(function(){
+//         curr = $(this).scrollTop();
 
-        if (curr > lastScroll) {
-            $('header').addClass('hide');
-        } else {
-            $('header').removeClass('hide');
-        }
+//         if (curr > lastScroll) {
+//             $('header').addClass('hide');
+//         } else {
+//             $('header').removeClass('hide');
+//         }
 
-        lastScroll = curr;
+//         lastScroll = curr;
 
-    })
+//     })
 
 
-    // 헤더 목록 리스트 버튼
-    $('.nav-list li').hover(function(){
-        $(this).children('.sub').addClass('show');
-    },function(){
-        $(this).children('.sub').removeClass('show');
-    })
+//     // 헤더 목록 리스트 버튼
+//     $('.nav-list li').hover(function(){
+//         $(this).children('.sub').addClass('show');
+//     },function(){
+//         $(this).children('.sub').removeClass('show');
+//     })
 
-})
+// })
